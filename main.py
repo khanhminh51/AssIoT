c9b43f69cb80cb102489cde5819fd96c014276fd
Minh PK <minh.phambku51@hcmut.edu.vn>
2024-06-10 17:24:02 +0700

    fix mqtt

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ temp = readTemperature()
 print(temp)
 humi = readMoisture()
 print(humi)
+while True:
+    if(publishdata("temp", temp)):
+        print("Publish temp success")
 
-if(publishdata("temp", temp)):
-    print("Publish temp success")
-
-if(publishdata("humi", humi)):
-    print("Publish humi success")
+    if(publishdata("humi", humi)):
+        print("Publish humi success")
+    
+    time.sleep(5)
